574d3e847e2f6a4a4fbe3b7049d2756e8f20e01b
Ahao-Try <1007843512@qq.com>
2024-03-06 23:36:53 +0800

    test1

---
+++ Intelligent-lab-system/build/CMakeFiles/Intelligent-lab-system.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "Intelligent-lab-system_autogen"
+  "CMakeFiles/Intelligent-lab-system_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/Intelligent-lab-system.dir/main.cpp.o"
+  "CMakeFiles/Intelligent-lab-system.dir/mainwindow.cpp.o"
+  "CMakeFiles/Intelligent-lab-system.dir/Intelligent-lab-system_autogen/mocs_compilation.cpp.o"
+  "Intelligent-lab-system.pdb"
+  "Intelligent-lab-system"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Intelligent-lab-system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Intelligent-lab-system/build/CMakeFiles/Intelligent-lab-system_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Intelligent-lab-system_autogen"
+  "CMakeFiles/Intelligent-lab-system_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/Intelligent-lab-system_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Intelligent-lab-system_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
